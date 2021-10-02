094df0239f01b418ef1fbc1960129ad2adf97b54
우상범 <leo429@naver.com>
2021-10-02 17:05:30 +0900

    Create if.py

---
+++ if.py
@@ -0,0 +1,16 @@
+money = True
+if money:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+
+if money:
+    print("택시를")
+    print("타고")
+    print("가라")
+
+money = 2000
+if money >= 3000:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
